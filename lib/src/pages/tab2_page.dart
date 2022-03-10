c9ad49e561820993fe587801ce15c826c16324de
rhernandezr <rhernandezr@jumapacelaya.gob.mx>
2022-03-10 15:40:56 -0600

    Practica terminada

--- lib/src/pages/tab2_page.dart
+++ lib/src/pages/tab2_page.dart
@@ -1,16 +1,112 @@
+import 'package:appnews/src/models/categoryModel.dart';
+import 'package:appnews/src/services/news_service.dart';
+import 'package:appnews/src/theme/tema.dart';
+import 'package:appnews/src/widgets/lista_noticias.dart';
 import 'package:flutter/material.dart';
-
-import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 
 class Tab2Screen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
-     ),
-   );
+
+    final newService = Provider.of<NewsService>(context);  
+
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+
+          children: [
+            
+            const _ListCategory(),
+            Expanded(
+
+              child: ( newService.getArticulosCategoriaSeleccionada!.length ==0)
+                     ? Center(child: CircularProgressIndicator(color: miTema.colorScheme.secondary,),) 
+                     : ListaNoticias(noticias: newService.getArticulosCategoriaSeleccionada!)
+              )
+
+          ],
+
+        )
+       ),
+    );
+  }
+}
+
+class _ListCategory extends StatelessWidget {
+  
+  const _ListCategory({Key? key,}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+   final categories = Provider.of<NewsService>(context).categories;   
+
+    return Container(
+      width: double.infinity,
+      height: 80,
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (BuildContext context, int index){
+
+            final categoryName = categories[index].name;
+
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  _CategoryButton(categoria: categories[index]),
+                  const SizedBox(height: 5,),
+                  //Mostrar la primer letra de la categoría en mayúsculas
+                  Text('${categoryName[0].toUpperCase()}${categoryName.substring(1)}')
+                ],
+              ),
+              
+              );
+        },
+      ),
+    );
+  }
+}
+
+class _CategoryButton extends StatelessWidget {
+  
+  final Category categoria;
+
+  const _CategoryButton({Key? key, required this.categoria, }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final newsService = Provider.of<NewsService>(context);
+
+    return GestureDetector(
+      
+      child: Container(
+        width: 40,
+        height: 40,
+        margin: const EdgeInsets.symmetric(horizontal: 10),
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+        ),
+        child: Icon(
+          categoria.icon,
+          color: (categoria.name == newsService.getSelectedCategory)
+                 ? miTema.colorScheme.secondary
+                 : Colors.black54,
+        ),
+      ),
+
+      onTap:(){
+          final newsService = Provider.of<NewsService>(context,listen: false);
+          newsService.setSelectedCategory = categoria.name;
+      },
+
+    );
   }
 }
--- lib/src/services/news_service.dart
+++ lib/src/services/news_service.dart
@@ -1,7 +1,7 @@
-import 'dart:ffi';
-
+import 'package:appnews/src/models/categoryModel.dart';
 import 'package:flutter/material.dart';
 import 'package:appnews/src/models/newsModels.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
 
 const _URL_BASE = 'https://newsapi.org/v2';
@@ -11,17 +11,59 @@ class NewsService with ChangeNotifier{
 
   List<Article> headlines = [];
 
+  //Categoría seleccionada, por default es business
+  String _selectedCategory = 'business';
+
+  List<Category> categories = [
+      Category(FontAwesomeIcons.building,        'business'),
+      Category(FontAwesomeIcons.tv,              'entertainment'),
+      Category(FontAwesomeIcons.addressCard,     'general'),
+      Category(FontAwesomeIcons.headSideVirus,   'health'),
+      Category(FontAwesomeIcons.vials,           'science'),
+      Category(FontAwesomeIcons.volleyballBall,  'sports'),
+      Category(FontAwesomeIcons.memory,          'technology'),  
+  ];
+
+ //Crear un map, para almacenar los datos cargados y tenerlos en cache, dando la sensación de que la app es mas rapida
+  Map<String, List<Article>> categoryArticles ={};
+
   NewsService(){
 
     getTopHeadLines();
 
+    //Inicializar el Map categoryArticles
+    //recorrer el arreglo de las categorias
+    categories.forEach((element) { 
+        //Pasamos el nombre de la categoria al  categoryArticles
+        categoryArticles[element.name] = new List.empty();
+    });
+
+    //Para cargar por default el busines
+    this.getArticlesByCategory(this._selectedCategory);
+
   }
 
+  //getters y setters
+  get getSelectedCategory => this._selectedCategory;
 
+  set setSelectedCategory(String categoria){
+      this._selectedCategory = categoria;
+
+      //Hacer la petición de la categoria seleccionada
+      getArticlesByCategory(categoria);
+
+      //Avisar que cambio la categoría
+      notifyListeners();
+  }
+
+  //Métodos
   getTopHeadLines() async{
     //url del End point
 
     const url = '$_URL_BASE/top-headlines?apiKey=$_API&country=mx';
+
+    //Hacer la petición
+    final resp = await http.get(Uri.parse(url));
 
     /*
     Otra manera
@@ -30,9 +72,6 @@ class NewsService with ChangeNotifier{
     final resp = await http.get(uri); 
     */
 
-    //Hacer la petición
-    final resp = await http.get(Uri.parse(url));
-
     //El body contiene la respuesta de la petición
     final newsResponse = newsResponseFromJson(resp.body);
 
@@ -40,5 +79,38 @@ class NewsService with ChangeNotifier{
     notifyListeners();
 
   }
+
+  getArticlesByCategory( String category) async{
+
+    if(categoryArticles[category]!.length>0){
+      
+      return this.categoryArticles[category];
+
+    } else{
+
+
+   /* final url = '$_URL_BASE/top-headlines?apiKey=$_API&country=mx&category=$category';
+    //Hacer la petición
+    final resp = await http.get(Uri.parse(url));*/
+
+    var uri = Uri.https('newsapi.org', '/v2/top-headlines',{
+      'country' : 'mx', 
+      'apiKey'  : '$_API',
+      'category': '$category'
+      });
+    
+    final resp = await http.get(uri); 
+
+    //El body contiene la respuesta de la petición
+    final newsResponse = newsResponseFromJson(resp.body);
+
+    //this.categoryArticles[category]?.addAll(newsResponse.articles);
+    this.categoryArticles[category] = newsResponse.articles;
+
+    }
+    notifyListeners();
+  }
+
+  List<Article>? get getArticulosCategoriaSeleccionada =>  this.categoryArticles[_selectedCategory];
 
 }
